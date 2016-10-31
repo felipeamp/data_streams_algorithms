b80abdecc0d5e84d39408a23143565454e14acee
Guilherme G. Schardong <schardong.gg@gmail.com>
2016-10-31 16:59:42 -0200

    Calculating the surprise number using the exact method. Added a return variable to the download_dataset function in order to indicate if the dataset was downloaded or locally present.

--- surprise_number.py
+++ surprise_number.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
 import collections
 import gzip
 import os
@@ -9,10 +8,18 @@ import sys
 import urllib.request
 
 def main():
-    dataset_filename = download_dataset()
-    calc_surprise_number(dataset_filename)    
+    downloaded, dataset_filename = download_dataset()
+    if downloaded:
+        print('Dataset downloaded.')
+    else:
+        print('Dataset locally present.')
+        
+    exact_sn = calc_surprise_number(dataset_filename, method = 'exact')
+    ans_sn = calc_surprise_number(dataset_filename, method = 'ans')
+    print('Surprise number (exact method) = '.format(exact_sn))
+    print('Surprise number (ans method) = '.format(ans_sn))
 
-def download_dataset(dataset_path = os.getcwd() + "/datasets"):
+def download_dataset(dataset_path = os.getcwd() + '/datasets'):
     """This function downloads the input dataset to the given path.
 
     Keyword arguments:
@@ -20,38 +27,31 @@ def download_dataset(dataset_path = os.getcwd() + "/datasets"):
     'datasets' folder under the current directory. The backslash will be appended
     automatically.
     """
-    dataset_filename = dataset_path + "/nytimes.txt.gz"
+    downloaded = False
+    dataset_filename = dataset_path + '/nytimes.txt.gz'
     if (os.path.exists(dataset_filename) == False or os.path.isfile(dataset_filename) == False):
-        url = "https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.nytimes.txt.gz"
+        url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.nytimes.txt.gz'
         urllib.request.urlretrieve(url = url,
                                    filename = dataset_filename)
-    return dataset_filename
+        downloaded = True
+    return downloaded, dataset_filename
 
-def calc_surprise_number(dataset_filename, method = "exact"):
-    with gzip.open(dataset_filename, 'r') as file_input:
-        num_docs = int(file_input.readline().decode('ascii').split('=')[0])
-        num_unique_words = int(file_input.readline().decode('ascii').split('=')[0])
-        num_words = int(file_input.readline().decode('ascii').split('=')[0])
-
-        i = 0
-        for line in file_input:
-            doc_id, word_id, count = line.decode('ascii').split(' ')
-            print('{}'.format(count))
-            i = i + 1
-            if i >= 50:
-                break
-    
+def calc_surprise_number(dataset_filename, method = 'exact'):
+    surprise_number = 0
     if (method == 'exact'):
-        pass
-    if (method == 'AMS'):
-        pass
-    return 0
+        with gzip.open(dataset_filename, 'r') as file_input:
+            num_docs = int(file_input.readline().decode('ascii').split('=')[0])
+            num_unique_words = int(file_input.readline().decode('ascii').split('=')[0])
+            num_words = int(file_input.readline().decode('ascii').split('=')[0])
 
-def moment_exact(dataset):
-    pass
-
-def moment_ams(dataset):
-    pass
+            for line in file_input:
+                doc_id, word_id, count = line.decode('ascii').split(' ')
+                surprise_number = surprise_number + int(count) ** 2
+                
+    elif (method == 'AMS'):
+        pass
+    
+    return surprise_number
 
 if __name__ == '__main__':
     main()
