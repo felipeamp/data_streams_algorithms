0ade0c365338e78f3da73e00d6353ec9adff6895
Guilherme G. Schardong <schardong.gg@gmail.com>
2016-10-31 12:57:06 -0200

    Added code to open the file returned by the download_dataset function. Added code to iterate through the lines of the file and parse each one of them. Testing for the first 50 lines for now.

--- surprise_number.py
+++ surprise_number.py
@@ -9,7 +9,8 @@ import sys
 import urllib.request
 
 def main():
-    download_dataset()
+    dataset_filename = download_dataset()
+    calc_surprise_number(dataset_filename)    
 
 def download_dataset(dataset_path = os.getcwd() + "/datasets"):
     """This function downloads the input dataset to the given path.
@@ -19,18 +20,38 @@ def download_dataset(dataset_path = os.getcwd() + "/datasets"):
     'datasets' folder under the current directory. The backslash will be appended
     automatically.
     """
-    dataset_file = dataset_path + "/nytimes.txt.gz"
-    if (os.path.exists(dataset_file) == False or os.path.isfile(dataset_file) == False):
+    dataset_filename = dataset_path + "/nytimes.txt.gz"
+    if (os.path.exists(dataset_filename) == False or os.path.isfile(dataset_filename) == False):
         url = "https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.nytimes.txt.gz"
         urllib.request.urlretrieve(url = url,
-                                   filename = dataset_file)
+                                   filename = dataset_filename)
+    return dataset_filename
 
-def calc_surprise_number(dataset_file, method = "exact"):
-    if (method == "exact"):
+def calc_surprise_number(dataset_filename, method = "exact"):
+    with gzip.open(dataset_filename, 'r') as file_input:
+        num_docs = int(file_input.readline().decode('ascii').split('=')[0])
+        num_unique_words = int(file_input.readline().decode('ascii').split('=')[0])
+        num_words = int(file_input.readline().decode('ascii').split('=')[0])
+
+        i = 0
+        for line in file_input:
+            doc_id, word_id, count = line.decode('ascii').split(' ')
+            print('{}'.format(count))
+            i = i + 1
+            if i >= 50:
+                break
+    
+    if (method == 'exact'):
         pass
-    if (method == "AMS"):
+    if (method == 'AMS'):
         pass
     return 0
+
+def moment_exact(dataset):
+    pass
+
+def moment_ams(dataset):
+    pass
 
 if __name__ == '__main__':
     main()
