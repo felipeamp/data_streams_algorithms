1e9038d28a0bef7e487612dec4f67f31e7b1299c
Guilherme G. Schardong <schardong.gg@gmail.com>
2016-11-04 17:39:29 -0200

    Partially fixed the AMS algorithm. Implementing the reservoir sampling algorithm, still need to implement the addition of a new stream element to the samples dictionary.

--- surprise_number.py
+++ surprise_number.py
@@ -7,11 +7,11 @@
 
 import zipfile
 import os
-import urllib.request
 import random
 import re
 import string
-
+import numpy as np
+import urllib.request
 
 def main():
     """
@@ -26,10 +26,10 @@ def main():
     else:
         print('Dataset locally present.')
 
-    exact_sn = calc_surprise_number_exact(dataset_filename)
-    #ans_sn = calc_surprise_number_ams(dataset_filename, [x/100 for x in range(1, 100)])
+    #exact_sn = calc_surprise_number_exact(dataset_filename)
+    ans_sn = calc_surprise_number_ams(dataset_filename, 10)
 
-    print('Surprise number (exact method) = {}'.format(exact_sn))
+    #print('Surprise number (exact method) = {}'.format(exact_sn))
     #print('Surprise numbers estimated using the ANS algorithm.')
     #for sn in ans_sn:
     #    print('Surprise number using {} of the values as variables = {}'.format(sn[1]))
@@ -40,7 +40,7 @@ def main():
 
 def download_dataset(url, filename, dataset_path=os.getcwd() + '/datasets'):
     """
-    This function downloads the input dataset to the given path.
+    This function downloads the input dataset to a local file.
 
     Keyword arguments:
     url -- The origin URL to download the dataset file.
@@ -52,7 +52,7 @@ def download_dataset(url, filename, dataset_path=os.getcwd() + '/datasets'):
     Returns:
     downloaded -- True if the dataset was downloaded, or False if it was locally
     present in the given folder.
-    dataset_filename -- The full path to the dataset, including it's given name.
+    dataset_filename -- The full path to the dataset, including its given name.
     """
     downloaded = False
     dataset_filename = dataset_path + '/' + filename
@@ -70,12 +70,12 @@ def calc_surprise_number_exact(dataset_filename):
     The surprise number is defined as the second moment of a set. The exact way
     to calculate it involves counting the number of occurrences of each i-th
     element of a dataset (named as m_i), squaring them (m_i^2) and summing the
-    resulting values (sum(m_i^2)). This method is exact, however, since the whole
-    dataset must be transversed, it is also slow.
+    resulting values (sum(m_i^2)). This method is exact, however it is slow,
+    since the whole dataset must be transversed.
 
     Arguments:
     dataset_filename -- The full path to the dataset file. This file is expected
-    to be in txt.gz format.
+    to be in .zip format.
 
     Returns:
     The 2nd moment, a.k.a. surprise number, of the dataset.
@@ -111,7 +111,7 @@ def calc_surprise_number_exact(dataset_filename):
 
     return surprise_number
 
-def calc_surprise_number_ams(dataset_filename, num_variables, sample_size):
+def calc_surprise_number_ams(dataset_filename, sample_size):
     """
     This function estimates the 2nd moment of a dataset (a.k.a. surprise number)
     using the Alon-Matias-Szegedy (AMS) algorithm.
@@ -119,43 +119,74 @@ def calc_surprise_number_ams(dataset_filename, num_variables, sample_size):
     Parameters:
     dataset_filename -- The full path to the dataset file. This file is expected
     to be in txt.gz format.
-    num_variables -- The number of variables to be chosen by the algorithm.
     sample_size -- The size of the sample to be used in the reservoir sampling
     algorithm. This number must be smaller than the size of the dataset.
 
     Returns:
     """
 
-    # Reading the whole file to the memory.
+    # Since the file is small, we read it into the memory.
     file_contents = []
-    with gzip.open(dataset_filename, 'r') as file_input:
+    with zipfile.ZipFile(dataset_filename, 'r').open('Norvig.txt', 'r') as file_input:
         file_contents = file_input.readlines()
 
-    # Decoding the lines of the file.
+    # Decoding the file's contents line-by-line.
     for line_idx, line in enumerate(file_contents):
         file_contents[line_idx] = line.decode('ascii')
 
-    num_unique_words = int(file_contents[1])
-    num_words = int(file_contents[2])
-    avg_surprise_numbers = []
-    for perc in perc_num_variables:
-        num_variables = int(perc * num_unique_words)
-        # The +3 factor is due to the fact that the file header is composed of 3
-        # lines and we don't want them being picked by our sampling.
-        var = random.sample(list(range(3, num_unique_words + 3)),
-                            num_variables)
+    sample_dict = {}
+    count_dict = {}
+    stream_elements_visited = 0
 
-        surprise_numbers = []
-        for curr_var in range(num_variables):
-            _, _, count = file_contents[var[curr_var]].split(' ')
-            count = int(count)
-            ams = num_words * (2 * count - 1)
-            surprise_numbers.append(ams)
+    for line in file_contents:
+        # Splitting the line by punctuation and whitespace characters.
+        split_line = re.split('[' + string.punctuation + string.whitespace + ']',
+                              line)
+        split_line = list(filter(None, split_line))
+        for token in split_line:
+            # If the number of stream elements visited is less than the sample
+            # size, the element is automatically included in the samples
+            # dictionary.
+            if stream_elements_visited < sample_size:
+                # If the element is not in the dictionary, we add it to the
+                # counter dict and the samples dict, else, we add another
+                # counter to the count dict and increment all counters in the
+                # token dict inside the samples dict.
+                if not (token in count_dict):
+                    count_dict[token] = 1
+                    sample_dict[token] = {0: 1}
+                else:
+                    for k, v in sample_dict[token].items():
+                        sample_dict[token][k] = v + 1
+                    sample_dict[token][count_dict[token]] = 1
+                    count_dict[token] = count_dict[token] + 1
+            else:
+                if random.random() > sample_size / stream_elements_visited:
+                    # If the random number generator returns a value larger
+                    # than sample_size / N, then we must add the new stream
+                    # element to our sample. Since our sample is full, we must
+                    # choose an element to remove from it. To remove an element,
+                    # we choose one uniformly at random, remove it and add the
+                    # new element to our sample set.
+                    elem = np.random.choice(a=list(sample_dict.keys()),
+                                            p=[len(v.keys()) / sum(count_dict.values()) for _, v in sample_dict.items()],
+                                            replace=False)
+                    if len(sample_dict[elem]) == 1:
+                        del(sample_dict[elem])
+                        del(count_dict[elem])
+                    else:
+                        to_del = np.random.choice(a=list(sample_dict[elem].keys()))
+                        del(to_del)
 
-        estimated_sn = sum(surprise_numbers) / float(num_variables)
-        avg_surprise_numbers.append((perc, estimated_sn))
+                    ## Adding the new element to the sample.
+                    if token in sample_dict:
+                        pass
+                    else:
+                        pass
+                    
+            
+            stream_elements_visited = stream_elements_visited + 1
 
-    return avg_surprise_numbers
 
 if __name__ == '__main__':
     main()
