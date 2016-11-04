4b0d05ad0f41065bdd69edc02c6af6549709b792
Guilherme G. Schardong <schardong.gg@gmail.com>
2016-11-04 12:30:28 -0200

    Fixed the dataset download and surprise number calculation functions. The functions are now working using the new datasets provided for this work.

--- surprise_number.py
+++ surprise_number.py
@@ -5,40 +5,46 @@
 2nd moments calculation using and exact method and the Alon-Matias-Szegedy (AMS) algorithm.
 """
 
-import gzip
+import zipfile
 import os
 import urllib.request
 import random
+import re
+import string
+
 
 def main():
     """
     Main function. Calls the 2nd moment calculation functions and analyzes the
     resulting values and time metrics.
     """
-    downloaded, dataset_filename = download_dataset()
+    fileurl = 'http://www-di.inf.puc-rio.br/~laber/ExpandedNrvg.zip'
+    downloaded, dataset_filename = download_dataset(url=fileurl,
+                                                    filename='norvig.zip')
     if downloaded:
         print('Dataset downloaded.')
     else:
         print('Dataset locally present.')
 
     exact_sn = calc_surprise_number_exact(dataset_filename)
-    ans_sn = calc_surprise_number_ams(dataset_filename, [x/100 for x in range(1, 100)])
+    #ans_sn = calc_surprise_number_ams(dataset_filename, [x/100 for x in range(1, 100)])
 
     print('Surprise number (exact method) = {}'.format(exact_sn))
-    print('Surprise numbers estimated using the ANS algorithm.')
-    for sn in ans_sn:
-        print('Surprise number using {} of the values as variables = {}'.format(sn[1]))
-    
-    for sn in ans_sn:
-        perc = sn[0] * 100
-        print('Ratio of exact SN and AMS SN using {} of the values as variables = {}'.format(perc, exact_sn / sn[1]))
+    #print('Surprise numbers estimated using the ANS algorithm.')
+    #for sn in ans_sn:
+    #    print('Surprise number using {} of the values as variables = {}'.format(sn[1]))
+    #for sn in ans_sn:
+    #    perc = sn[0] * 100
+    #    print('Ratio of exact SN and AMS SN using {} of the values as variables = {}'.format(perc, exact_sn / sn[1]))
 
 
-def download_dataset(dataset_path=os.getcwd() + '/datasets'):
+def download_dataset(url, filename, dataset_path=os.getcwd() + '/datasets'):
     """
     This function downloads the input dataset to the given path.
 
     Keyword arguments:
+    url -- The origin URL to download the dataset file.
+    filename -- The destination file name for the dataset.
     dataset_path -- The folder to save the dataset into. Default value is the
     'datasets' folder under the current directory. The backslash will be appended
     automatically.
@@ -46,13 +52,12 @@ def download_dataset(dataset_path=os.getcwd() + '/datasets'):
     Returns:
     downloaded -- True if the dataset was downloaded, or False if it was locally
     present in the given folder.
-    dataset_filename -- The full path to the dataset.
+    dataset_filename -- The full path to the dataset, including it's given name.
     """
     downloaded = False
-    dataset_filename = dataset_path + '/nytimes.txt.gz'
-    if os.path.exists(dataset_filename) is False or os.path.isfile(dataset_filename) is False:
+    dataset_filename = dataset_path + '/' + filename
+    if not os.path.exists(dataset_filename) or not os.path.isfile(dataset_filename):
         print('Downloading dataset.')
-        url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.nytimes.txt.gz'
         urllib.request.urlretrieve(url=url,
                                    filename=dataset_filename)
         downloaded = True
@@ -75,15 +80,34 @@ def calc_surprise_number_exact(dataset_filename):
     Returns:
     The 2nd moment, a.k.a. surprise number, of the dataset.
     """
-    surprise_number = 0
-    with gzip.open(dataset_filename, 'r') as file_input:
-        _ = int(file_input.readline().decode('ascii').split('=')[0])
-        _ = int(file_input.readline().decode('ascii').split('=')[0])
-        _ = int(file_input.readline().decode('ascii').split('=')[0])
 
-        for line in file_input:
-            _, _, count = line.decode('ascii').split(' ')
-            surprise_number = surprise_number + int(count) ** 2
+    # Since the file is small, we read it into the memory.
+    file_contents = []
+    with zipfile.ZipFile(dataset_filename, 'r').open('Norvig.txt', 'r') as file_input:
+        file_contents = file_input.readlines()
+
+    # Decoding the file's contents line-by-line.
+    for line_idx, line in enumerate(file_contents):
+        file_contents[line_idx] = line.decode('ascii')
+
+    ## Building the word dictionary.
+    token_dict = {}
+    for line in file_contents:
+        ## Splitting the line by punctuation and whitspace characters, however,
+        ## some empty strings are returned, thus the call to the 'filter'
+        ## function.
+        split_line = re.split('[' + string.punctuation + string.whitespace + ']', line)
+        split_line = list(filter(None, split_line))
+        for token in split_line:
+            if token in token_dict:
+                token_dict[token] = token_dict[token] + 1
+            else:
+                token_dict[token] = 1
+
+    # Calculating the surprise number.
+    surprise_number = 0
+    for _, count in token_dict.items():
+        surprise_number = surprise_number + count ** 2
 
     return surprise_number
 
